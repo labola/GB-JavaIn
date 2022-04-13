6fb3a8d14c32f59f525a77c7fa720228545f6ff4
labola <labola2016@yandex.ru>
2022-04-13 21:56:44 +0300

    added HW7

--- HW1/src/main/java/org/example/HomeWorkApp.java
+++ HW1/src/main/java/org/example/HomeWorkApp.java
@@ -2,18 +2,6 @@ package org.example;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
-        Dog dog1=new Dog("Bobik");
-        dog1.getinfo();
-        dog1.run(150);
-        Dog dog2=new Dog("Zhorik");
-        dog2.swim(10);
-        Cat cat1 = new Cat("Goga");
-        cat1.run(199);
-        cat1.swim(10);
-        dog1.getCounter();
-        cat1.getCounter();
-        Animal an = new Animal("Noname");
-        an.getCounter();
-
+    
     }
 }
